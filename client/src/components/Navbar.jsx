558db7488e0b2003e5a02f6ba88485ef892f8f0f
imadJHR <imadjohar4@gmail.com>
2025-01-06 22:52:22 +0100

    to netlify

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -137,7 +137,7 @@ export function Navbar({ cartCount, onCartClick }) {
                         mobile
                       />
                       <Link
-                        to="/products/silicone-moule"
+                        to="/products"
                         className="text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
                         onClick={handleLinkClick}
                       >
